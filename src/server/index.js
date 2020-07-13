f2e014d61877be1816ae353730866e4c976e3280
Amal Salvin <amalsalvin97@gmail.com>
2020-07-13 09:47:09 +0530

    config changes

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,20 @@
-const express = require('express');
-const os = require('os');
+const express = require("express");
+const os = require("os");
+require('dotenv').config();
 
 const app = express();
 
-app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+app.listen(process.env.PORT || 8080, () =>
+  console.log(`Listening on port ${process.env.PORT || 8080}!`)
+);
 
-app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+//for production
+app.use(express.static("dist"));
+
+//routes
+app.get("/api/getUsername", (req, res) =>  
+  res.send({ username: os.userInfo().username })
+);
+
+
+
