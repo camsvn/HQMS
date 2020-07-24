cfc43270c41e484111c44acf5bbc2ea295c7cb3f
Amal Salvin <amalsalvin97@gmail.com>
2020-07-24 20:27:00 +0530

    Routing Kazhinju

--- src/client/components/DBody.js
+++ src/client/components/DBody.js
@@ -1,24 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import socketIOClient from "socket.io-client";
 import mockData from "../constants/mockData";
 
+var socket;
+const ENDPOINT = "http://desktop-8560w:8080/";
+
 export default function Dispay_Body() {
+  const [doctors, setDoctors] = useState([]);
+
+  useEffect(() => {
+    socket = socketIOClient(ENDPOINT);
+    socket.on("token-update", (data) => {
+      setDoctors(data.localDS);
+    });
+    return () => {
+      if (socket) socket.disconnect();
+    };
+  }, []);
   return (
     <div id="main-body">
-      {mockData.map((item) => (
+      {doctors.map((item) => (
         <div className="token-card" key={item.id}>
-          <img src={item.image} alt="Doctor DP" />
+          <img src={mockData[2].image} alt="Doctor DP" />
           <div>
             <h1>{item.name}</h1>
           </div>
           <div>
-            <span>22</span>
+            <span>{item.currentToken}</span>
           </div>
         </div>
       ))}
     </div>
   );
 }
-
 const styles = {
   main_body: {
     border: "10px solid yellowgreen",
